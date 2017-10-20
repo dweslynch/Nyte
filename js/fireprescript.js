8d6debb8cfb31a1951ffd73c16bafa1fa0b24574
Devin <devin-lynch@uiowa.edu>
2017-10-19 19:35:54 -0500

    initial commit

---
+++ js/fireprescript.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAm1LCGwMx46rYkWoXlJTTuGgcldKNETOw",
+  authDomain: "nyte-63b9f.firebaseapp.com",
+  databaseURL: "https://nyte-63b9f.firebaseio.com",
+  projectId: "nyte-63b9f",
+  storageBucket: "nyte-63b9f.appspot.com",
+  messagingSenderId: "824790007542"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
---
+++ js/postscript.js
@@ -0,0 +1,6 @@
+var w = $("#intro").css("width");
+$("input").css("width", w);
+$("select").css("width", w);
+
+var h = $("html").css("height");
+$("#backdrop").css("height", h);
