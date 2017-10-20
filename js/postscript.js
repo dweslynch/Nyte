9c4e19d65e53ab59e4fc3c88cf95dd35f5f46128
Devin <devin-lynch@uiowa.edu>
2017-10-20 18:31:05 -0500

    add user accounts

--- js/postscript.js
+++ js/postscript.js
@@ -1,6 +1,7 @@
+$("#intro").css("display", "inline-block");
 var w = $("#intro").css("width");
 $("input").css("width", w);
 $("select").css("width", w);
 
-var h = $("html").css("height");
-$("#backdrop").css("height", h);
+//var h = $("html").css("height");
+//$("#backdrop").css("height", h);
