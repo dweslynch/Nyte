b050e0c12554c78013d53a012400edc1a69f40ea
Devin <devin-lynch@uiowa.edu>
2017-10-23 10:47:48 -0500

    maybe fix dropdown

--- js/postscript.js
+++
@@ -1,7 +0,0 @@
-$("#intro").css("display", "inline-block");
-var w = $("#intro").css("width");
-$("input").css("width", w);
-$("select").css("width", w);
-
-//var h = $("html").css("height");
-//$("#backdrop").css("height", h);
